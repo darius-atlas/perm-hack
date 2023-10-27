7fd74dd34a34db0baa9c5173106751be656972d9
Artem-Darius Atlas <mit.3tlasa@gmail.com>
2023-10-27 14:19:59 +0300

    Init

---
+++ apps/py-conveyor-service/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'py-conveyor-service',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html', 'py'],
+  coverageDirectory: '../../coverage/apps/py-conveyor-service',
+};
