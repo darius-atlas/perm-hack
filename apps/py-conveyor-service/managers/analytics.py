96b4fed18cc34d7028744e36a4a6fcb27c3a92d1
Nikita <jb357506@gmail.com>
2023-10-28 05:14:32 +0300

    Fix route

--- apps/py-conveyor-service/managers/analytics.py
+++ apps/py-conveyor-service/managers/analytics.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from db.models.conveer import DBConveer
 from db.repository.analytics import AnalyticsRepository
 
 
@@ -12,7 +13,7 @@ class AnalyticsManager:
     async def get_by_filters(cls,
                              session: AsyncSession,
                              start_date: Optional[datetime],
-                             end_date: Optional[datetime]):
-        datas = await AnalyticsRepository(session).get_analytic(start_date=start_date,
-                                                                end_date=end_date)
+                             end_date: Optional[datetime]) -> list[DBConveer]:
+        datas: list[DBConveer] = await AnalyticsRepository(session).get_analytic(start_date=start_date,
+                                                                                 end_date=end_date)
         return datas
--- apps/py-conveyor-service/server/routers/analytics.py
+++ apps/py-conveyor-service/server/routers/analytics.py
@@ -2,21 +2,23 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.request.analytics import AnalyticsFilters
+from api.response.analytics import ResponseAnalyticsFactory, ResponseAnalytics
+from db.models.conveer import DBConveer
 from managers.analytics import AnalyticsManager
-from server.depends import get_session, PagesPaginationParams
+from server.depends import get_session
 
 router = APIRouter(prefix="/api/analytics", tags=['Ride'])
 
 
-@router.post('/all')
+@router.post('/all', response_model=ResponseAnalytics)
 async def get_all_analytics(
     filters: AnalyticsFilters,
     session: AsyncSession = Depends(get_session),
 ):
-    data = await AnalyticsManager.get_by_filters(
+    data: list[DBConveer] = await AnalyticsManager.get_by_filters(
         session=session,
         start_date=filters.start_time,
         end_date=filters.end_time
     )
 
-    return data
+    return ResponseAnalyticsFactory.get_from_models(models=data)
