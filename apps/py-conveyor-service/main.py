44b2ba831f85fd647678aa6f9ed08e5608fc2d33
Nikita <jb357506@gmail.com>
2023-10-29 08:46:58 +0300

    update cors

--- apps/py-conveyor-service/main.py
+++ apps/py-conveyor-service/main.py
@@ -1,11 +1,22 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from server.routers_init import all_routers
 
 app = FastAPI(
     title="Конвеер",
     docs_url='/api/docs'
+)
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 for router in all_routers:
